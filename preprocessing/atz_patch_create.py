44963cbbe4658282c7462c674fa7f75a44a99b42
soumensardar <soumensardarintmain@gmail.com>
2023-01-08 01:12:13 +0530

    - generate image patches completed

--- preprocessing/atz_patch_create.py
+++ preprocessing/atz_patch_create.py
@@ -10,31 +10,46 @@ from tqdm import tqdm
 
 from preprocessing.utils import read_content
 
-atz_classes = ["UNKNOWN", "UN", "HUMAN",
-               "KK", "GA", "MD",
-               "SS", "WB", "CK",
-               "CP", "KC", "LW", "CL"]
-atz_ignore_classes = ["HUMAN", "MD"]
+class_name_dict = dict(KK="Kitchen Knife", GA="Gun", MD="Metal Dagger",
+                       SS="Scissors", WB="Water Bottle", CK="Ceramic Knife",
+                       CP="Cell Phone", KC="Key Chain", LW="Leather Wallet",
+                       CL="Cigarette Lighter", UN="Unknown", UNKNOWN="UNKNOWN",
+                       HUMAN="HUMAN",
+                       )
+# do not change this order
+atz_classes = ["NORMAL0", "NORMAL1", "HUMAN",
+               "UNKNOWN", "UN",
+               "CP", "MD", "GA", "KK", "SS",
+               "KC", "WB", "LW", "CK", "CL"]
+# index for a class from the `atz_classes` list. Any class upto 'SS'
+# in that list will be ignored during dataset creation
+atz_ignore_cls_idx_lim = atz_classes.index("WB")
 
 root = pathlib.Path("/Users/soumen/Downloads/Datasets/Active Terahertz Imaging Dataset")
 image_root = root / "THZ_dataset_det_VOC/JPEGImages"
 voc_root = root / "THZ_dataset_det_VOC/Annotations"
-display = True
+display = False  # display plots
 dataset_save_path = pathlib.Path("./patch_atz")
 patch_size = 64
-mask_iou = 0.3  # this will be used to
-patch_dataset_csv = str(dataset_save_path / ("atz_patch_dataset_iou%d_%d.csv" % (mask_iou, patch_size)))
-atz_patch_dataset_df = pd.DataFrame()
+mask_iou = 0.3  # this will be used to decide anomaly or not
+postfix = "_%d_%0.2f_%dx%d.csv" % (atz_ignore_cls_idx_lim + 1, mask_iou, patch_size, patch_size)
+patch_dataset_csv = str(dataset_save_path / ("atz_patch_dataset_%s.csv" % postfix))
 
 # create save directory
-os.makedirs(str(dataset_save_path / "images"), exist_ok=True)
+patch_image_save_path = dataset_save_path / "images"
+os.makedirs(str(patch_image_save_path), exist_ok=True)
+
+intersection = patch_size * patch_size
 
 
 def create_patch_dataset():
     # select image and annotation
     image_files = os.listdir(image_root)
+    atz_patch_dataset_df = pd.DataFrame()
     for image_name in tqdm(image_files):
-        image_name = "S_P_M5_MD_F_W_SS_V_W_back_0906085926.jpg"
+        if not any([c in image_name for c in atz_classes[atz_ignore_cls_idx_lim + 1:]]):
+            continue
+        # image_name = "S_P_M5_MD_F_W_SS_V_W_back_0906085926.jpg"
         voc_xml = voc_root / image_name.replace(".jpg", ".xml")
 
         # read annotation
@@ -48,18 +63,21 @@ def create_patch_dataset():
         # apply bbox and create mask
         for box_info in boxes:
             (xmin, ymin, xmax, ymax, cx, cy, class_) = box_info
-            if class_ in atz_ignore_classes:
+            cls_idx = atz_classes.index(class_)
+            if cls_idx <= atz_ignore_cls_idx_lim:
+                # ignore classes
                 continue
+            mask[ymin:ymax + 1, xmin:xmax + 1] = cls_idx
             if display:
                 x, y, w, h = xmin, ymin, abs(xmax - xmin), abs(ymax - ymin)
                 img = cv2.rectangle(img, (x, y), (x + w, y + h), (r, g, b), 4)
                 print("boxes:", class_, atz_classes.index(class_))
-            mask[ymin:ymax + 1, xmin:xmax + 1] = atz_classes.index(class_)
 
         if display:
             # render image
             cv2.imshow("image", img)
             cv2.imshow("mask", mask)
+            print(np.unique(mask))
 
         ## PATCHIFY
         # # create patches
@@ -86,41 +104,63 @@ def create_patch_dataset():
         emp = EMPatches()
         img_patches, indices = emp.extract_patches(img, patchsize=patch_size, overlap=0.2)
         mask_patches, _ = emp.extract_patches(mask, patchsize=patch_size, overlap=0.2)
-
+        # print(np.unique(mask))
         cols = 4
         rows = len(img_patches) // cols + 1
         for idx, (img_p, mask_p) in enumerate(zip(img_patches, mask_patches)):
+            # ignore a patch with all [black pixels
+            # img_p
             # create patch file name
-            patch_file_name = "%s.jpg" % image_name.replace(".jpg", "%04d" % idx)
+            patch_file_name = "%s.jpg" % image_name.replace(".jpg", "_%04d" % idx)
+            # class label
+            max_val = np.max(mask_p)
+            class_index = max_val
             # class name
-            class_label = np.max(mask_p)
+            label_txt = atz_classes[int(class_index)]
+            # if class_index > 0: print(label_txt, class_index, idx, np.unique(mask_p))
             # calculate IOU
+            # the entire patch is the union,
+            # and the masked area is intersection.
             mask_p[mask_p > 0] = 1  # replace all non zeros with 1
-            a1 = np.sum(mask_p.flatten())
-            a2 = patch_size * patch_size
-            iou = a1 / a2
-            if iou >= mask_iou:
+            union = np.sum(mask_p.flatten()) + 1e-20  # sum of 1s give mask area(the union)
+            iou = intersection / union
+            # compare iou
+            if 1 >= iou >= mask_iou:
                 # anomaly
-                class_label
-                ...
+                is_anomaly = True
             else:
                 # normal
-                ...
+                is_anomaly = False
+                label_txt = "NORMAL"
+
+            # prepare record
+            dictionary = dict(image=patch_file_name, patch_id=idx, label=class_index, label_txt=label_txt,
+                              anomaly=is_anomaly)
+            # update dataframe
+            df_dictionary = pd.DataFrame([dictionary])
+            atz_patch_dataset_df = pd.concat([atz_patch_dataset_df, df_dictionary], ignore_index=True)
+            # save image
+            cv2.imwrite(str(patch_image_save_path / patch_file_name), img_p)
             if display:
                 ax = plt.subplot(rows, cols, idx + 1)
                 ax.axis("off")
                 mask_rgb = mask_p.copy().astype('uint8')
                 mask_rgb[mask_rgb > 0] = 255
+                ax.set_title("%d" % idx, fontdict=dict(color="red"))
                 ax.imshow(np.hstack((img_p, mask_rgb)))
 
                 # ax = plt.subplot(rows, cols, idx + 2)
                 # ax.axis("off")
                 # ax.imshow(mask_p)
-        return
+        # save csv
+        atz_patch_dataset_df.columns = ["image", "patch_id", "label", "label_txt", "anomaly"]
+        atz_patch_dataset_df.to_csv(patch_dataset_csv)
 
 
 if __name__ == '__main__':
     create_patch_dataset()
+    print("Files are saved @", str(patch_image_save_path))
+    print("Metadata @", patch_dataset_csv)
     if display:
         plt.show()
         cv2.destroyAllWindows()
